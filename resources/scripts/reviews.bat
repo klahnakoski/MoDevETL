8b670090bd875af961464703f24c41b842cf6988
kyle lahnakoski <klahnakoski@mozilla.com>
2014-10-22 12:33:02 +0000

    longer timeout

--- resources/scripts/reviews.bat
+++ resources/scripts/reviews.bat
@@ -2,7 +2,7 @@ set PYTHONPATH=.
 
 start "private1" pypy MoDevETL\reviews.py --settings=resources/settings/private_review_settings_part1.json --reset
 start "public1" pypy MoDevETL\reviews.py --settings=resources/settings/public_review_settings_part1.json --reset
-timeout 120
+timeout 600
 start "private2" pypy MoDevETL\reviews.py --settings=resources/settings/private_review_settings_part2.json
 start "public2" pypy MoDevETL\reviews.py --settings=resources/settings/public_review_settings_part2.json
 

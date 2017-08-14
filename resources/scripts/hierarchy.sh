121d3381c294d1348b1c2c7fe8122a5790e361e7
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-14 17:45:50 -0400

    only private works for now

--- resources/scripts/hierarchy.sh
+++ resources/scripts/hierarchy.sh
@@ -1,3 +1,4 @@
 cd ~/hierarchy
 export PYTHONPATH=.
-python MoDevETL\hierarchy.py --settings=resources/settings/hierarchy_public.json
+python MoDevETL/hierarchy.py --settings=resources/settings/prod/hierarchy_private.json
+python MoDevETL/hierarchy.py --settings=resources/settings/prod/hierarchy_public.json
